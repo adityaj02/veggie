75ef239e7bd6b20fb4c84bdbe980b08442af3677
adityaj02 <adityajmarch020304@gmail.com>
2026-08-12 15:21:24 +0530

    fix: correctly import connect-mongo (TypeError: MongoStore.create is not a function)

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(cors({
 }));
 app.use(express.json({ limit: '10mb' }));
 
-const MongoStore = require('connect-mongo');
+const { MongoStore } = require('connect-mongo');
 
 // Setup Sessions
 app.use(session({
